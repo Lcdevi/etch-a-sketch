f7d3f312568c664ab6d9199eb3d4c4e323fb39fc
Lcdevi <caillauxlaura@gmail.com>
2020-04-09 19:01:05 +0200

    mouse hover + grid + prompt whith how many squares

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-/*
+let draw = document.getElementById("main");
+let playerChoice = Number(window.prompt("Enter a number", ""));;
+//let playerChoice = 50;
+//function to create new divs
 function createDiv() {
-
-  let div = document.createElement("div");
-  div.style.width = "100px";
-  div.style.height = "100px";
-  div.style.border = "solid 1px black";
-  div.style.background = "white";
-  div.style.color = "black";
-  div.innerHTML = "Hello";
-
-  document.getElementById("div1").appendChild(div);
-
-};
-
-createDiv();
-*/
-function createDiv() {
+let numberOfCases = playerChoice * playerChoice;
 let i;
-for (i = 0; i < 16; i++) {
+for (i = 0; i < numberOfCases; i++) {
+let size = (450/Math.sqrt(numberOfCases))-2;
+  let div = document.createElement("div");
 
-    let div = document.createElement("div");
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.border = "solid 1px black";
-    div.style.background = "white";
-    div.style.color = "black";
-    div.innerHTML = "Hello";
+    div.style.width = size + "px";
+    div.style.height = size +"px";
+    div.style.border = "solid 1px white";
+    div.style.background = "#fcf176";
 
-    document.getElementById("div1").appendChild(div);
+    document.getElementById("grid").appendChild(div);
 
+
+    console.log(size);
   }
+  draw.addEventListener("mouseover", function( event ) {
+    // on met l'accent sur la cible de mouseover
+    event.target.style.backgroundColor = "black";
+
+  }, false);
+
 }
 
 createDiv();
