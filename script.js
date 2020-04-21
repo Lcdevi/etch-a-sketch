5babbc187779c7037963f903eb3786e948a7b155
Lcdevi <caillauxlaura@gmail.com>
2020-04-21 17:58:55 +0200

    clean the code

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-let playerChoice2 = Number(window.prompt("Enter a number", ""));
-let totalCases = playerChoice2 * playerChoice2;
-let minus = playerChoice2-1;
-let size2 = (450/playerChoice2);
+let playerChoice = Number(window.prompt("Enter a number", ""));
+let totalCases = playerChoice * playerChoice;
+let minus = playerChoice-1;
+let size = (450/playerChoice);
 let gridexjs = document.getElementById("gridex");
 gridexjs.style.display = "grid";
 gridexjs.style.width = "450px";
 gridexjs.style.height="450px";
-gridexjs.style.gridTemplateColumns= "repeat("+minus+", " + size2 + "px) 1fr";
-gridexjs.style.gridTemplateRows= "repeat("+playerChoice2+", " + size2 + "px) 1fr";
+gridexjs.style.gridTemplateColumns= "repeat("+minus+", " + size + "px) 1fr";
+gridexjs.style.gridTemplateRows= "repeat("+playerChoice+", " + size + "px) 1fr";
 
 function createDiv2 () {
   let i;
@@ -25,6 +25,6 @@ gridexjs.addEventListener("mouseover", function( event ) {
   event.target.style.backgroundColor = "black";
 
 }, false);
-console.log(size2);
+console.log(size);
 console.log(minus);
 console.log(totalCases);
